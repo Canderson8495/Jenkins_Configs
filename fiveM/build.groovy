633c74348595be743b274287137d285b4fec50c0
Cameron Anderson <Thrillechs@gmail.com>
2022-08-08 20:14:53 -0400

    Update build.groovy

--- fiveM/build.groovy
+++ fiveM/build.groovy
@@ -7,7 +7,7 @@ node {
 
     stage('Build image') {
         /* Build your image */
-        app = docker.build("fivem", "/tmp/build-${BUILD_NUMBER}")
+        app = docker.build("fivem", "/tmp/${BUILD_TAG}")
     }
 
     stage('Push image') {
@@ -16,28 +16,5 @@ node {
             app.push("${branch}")
         }
     }
-
-    stage("Prepare"){
-        try {
-            sh 'docker-compose -p Cluster_1 stop'
-            sh 'docker-compose -p Cluster_1 rm -f'
-            echo "Removed old containers"
-        } catch (Exception e) {
-            echo 'Returned: ' + e.toString()
-        }
-    }
-
-    stage('Pull Image') {
-        sh 'docker pull localhost:5000/fivem:${branch}'
-    }
-    
-    stage('deploy') {
-        //sh 'docker run -d --name C1 localhost:9999/fivem:${branch}'
-        sh 'git clone https://github.com/Canderson8495/fivem-1.git /tmp/deploy-${BUILD_NUMBER}'
-        sh 'ls /tmp/deploy-${BUILD_NUMBER}'
-        sh 'cd /tmp/deploy-${BUILD_NUMBER} && sudo docker-compose -p Cluster_1 up -d'
-        sh "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' fivem"
-    }
-    
 }
 
